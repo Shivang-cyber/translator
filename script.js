68482b1052cb29dc7ad88d00096cdbf91e6e3605
Shivang <maneyrocks@gmail.com>
2021-09-07 22:50:28 +0530

    first push v1.0.0

---
+++ script.js
@@ -0,0 +1,23 @@
+async function tra() {
+ var a = document.getElementById('IN').value
+ var b = document.getElementById('OUT')
+
+ var aFR = document.getElementById('F-s').value
+ var bTO = document.getElementById('T-s').value
+
+ console.log(a,aFR,bTO);
+
+var res = await fetch('https://libretranslate.de/translate', {
+  method: 'POST',
+  body: JSON.stringify({
+    q: a,
+    source: aFR,
+    target: bTO,
+  }),
+  headers: { 'Content-Type': 'application/json' },
+})
+
+var data = await res.json()
+
+b.innerHTML = data.translatedText
+}
